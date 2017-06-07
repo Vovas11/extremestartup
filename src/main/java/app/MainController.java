e9c309cdc7a7bca07befd051f43d7175d64962ee
Vovas11 <Vovas11@users.noreply.github.com>
2017-06-07 15:10:21 +0200

    Change to RestController

--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -1,22 +1,14 @@
 package app;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@EnableAutoConfiguration
+@RestController
 public class MainController {
     
-    @RequestMapping("/")
-    @ResponseBody
+    @GetMapping
     String home() {
-        return "Hello World!";
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(MainController.class, args);
+        return "Hellloooo World!";
     }
 
 }
